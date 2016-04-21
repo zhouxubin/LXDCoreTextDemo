d260c3b6add53503712cbeccc2de664015944f78
Sindri Lin <634893985@qq.com>
2016-04-21 11:38:18 +0800

    Initial Commit

---
+++ LXDCoreTextDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LXDCoreTextDemo
+//
+//  Created by 林欣达 on 16/4/21.
+//  Copyright © 2016年 CNPay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LXDCoreTextDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LXDCoreTextDemo
+//
+//  Created by 林欣达 on 16/4/21.
+//  Copyright © 2016年 CNPay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
